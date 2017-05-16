294c1766890a13ceeffeda2b423233ebd92d6b1e
gopherstein <spankenstien@gmail.com>
2017-05-16 17:22:15 -0500

    WIP

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -12,6 +12,7 @@ import (
 	"github.com/supergiant/supergiant/pkg/provider/gce"
 	"github.com/supergiant/supergiant/pkg/provider/kubernetes"
 	"github.com/supergiant/supergiant/pkg/provider/openstack"
+	"github.com/supergiant/supergiant/pkg/provider/packet"
 	"github.com/supergiant/supergiant/pkg/server"
 )
 
@@ -57,6 +58,12 @@ func main() {
 			return &gce.Provider{
 				Core:   c,
 				Client: gce.Client,
+			}
+		}
+		c.PACKProvider = func(creds map[string]string) core.Provider {
+			return &packet.Provider{
+				Core:   c,
+				Client: packet.Client,
 			}
 		}
 
